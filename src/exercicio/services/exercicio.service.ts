cd303322b813869a3f64a77646b4dfb5fea319ba
Eduarda <contatoeduardagaleno@outlook.com>
2025-01-22 13:55:07 -0300

    CRUD Exercício

---
+++ src/exercicio/services/exercicio.service.ts
@@ -0,0 +1,52 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { Exercicio } from "../entities/exercicio.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult, Repository } from "typeorm";
+
+@Injectable()
+export class ExercicioService {
+
+    constructor(
+        @InjectRepository(Exercicio)
+        private exercicioRepository: Repository<Exercicio>
+    ) {}
+
+    async findAll(): Promise<Exercicio[]> {
+        return this.exercicioRepository.find({
+            relations: {
+                categoria: true
+            }
+        });
+    }
+
+    async findById(id: number): Promise<Exercicio> {
+        const exercicio = await this.exercicioRepository.findOne({
+            where: {
+                id
+            },
+            relations: {
+                categoria: true
+            }
+        });
+
+        if (!exercicio) {
+            throw new HttpException('Exercício não encontrado!', HttpStatus.NOT_FOUND);
+        }
+
+        return exercicio;
+    }
+
+    async create(exercicio: Exercicio): Promise<Exercicio> {
+        return this.exercicioRepository.save(exercicio);
+    }
+
+    async update(exercicio: Exercicio): Promise<Exercicio> {
+        await this.findById(exercicio.id);
+        return this.exercicioRepository.save(exercicio);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+        await this.findById(id);
+        return this.exercicioRepository.delete(id);
+    }
+}
