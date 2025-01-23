1ec1b72620de2106c21aee02bc23c707e576778c
ustavoteles <telesgustavo.dev@gmail.com>
2025-01-23 16:25:08 -0300

    Subindo o projeto para a main

--- src/exercicio/services/exercicio.service.ts
+++ src/exercicio/services/exercicio.service.ts
@@ -1,52 +1,54 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { Exercicio } from "../entities/exercicio.entity";
-import { InjectRepository } from "@nestjs/typeorm";
-import { DeleteResult, Repository } from "typeorm";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Exercicio } from '../entities/exercicio.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 
 @Injectable()
 export class ExercicioService {
+  constructor(
+    @InjectRepository(Exercicio)
+    private exercicioRepository: Repository<Exercicio>,
+  ) {}
 
-    constructor(
-        @InjectRepository(Exercicio)
-        private exercicioRepository: Repository<Exercicio>
-    ) {}
+  async findAll(): Promise<Exercicio[]> {
+    return this.exercicioRepository.find({
+      relations: {
+        categoria: true,
+      },
+    });
+  }
 
-    async findAll(): Promise<Exercicio[]> {
-        return this.exercicioRepository.find({
-            relations: {
-                categoria: true
-            }
-        });
+  async findById(id: number): Promise<Exercicio> {
+    const exercicio = await this.exercicioRepository.findOne({
+      where: {
+        id,
+      },
+      relations: {
+        categoria: true,
+      },
+    });
+
+    if (!exercicio) {
+      throw new HttpException(
+        'Exercício não encontrado!',
+        HttpStatus.NOT_FOUND,
+      );
     }
 
-    async findById(id: number): Promise<Exercicio> {
-        const exercicio = await this.exercicioRepository.findOne({
-            where: {
-                id
-            },
-            relations: {
-                categoria: true
-            }
-        });
+    return exercicio;
+  }
 
-        if (!exercicio) {
-            throw new HttpException('Exercício não encontrado!', HttpStatus.NOT_FOUND);
-        }
+  async create(exercicio: Exercicio): Promise<Exercicio> {
+    return this.exercicioRepository.save(exercicio);
+  }
 
-        return exercicio;
-    }
+  async update(exercicio: Exercicio): Promise<Exercicio> {
+    await this.findById(exercicio.id);
+    return this.exercicioRepository.save(exercicio);
+  }
 
-    async create(exercicio: Exercicio): Promise<Exercicio> {
-        return this.exercicioRepository.save(exercicio);
-    }
-
-    async update(exercicio: Exercicio): Promise<Exercicio> {
-        await this.findById(exercicio.id);
-        return this.exercicioRepository.save(exercicio);
-    }
-
-    async delete(id: number): Promise<DeleteResult> {
-        await this.findById(id);
-        return this.exercicioRepository.delete(id);
-    }
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findById(id);
+    return this.exercicioRepository.delete(id);
+  }
 }
