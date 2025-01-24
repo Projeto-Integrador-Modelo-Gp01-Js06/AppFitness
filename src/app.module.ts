4f93c678d7b574e753fe8e51f46b9efa16bd6970
ustavoteles <telesgustavo.dev@gmail.com>
2025-01-23 22:57:05 -0300

    Deploy do Projeto Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,23 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ExercicioModule } from "./exercicio/exercicio.module";
-import { Exercicio } from "./exercicio/entities/exercicio.entity";
-import { UsuarioModule } from "./usuario/usuario.module";
-import { Usuario } from "./usuario/entities/usuario.entity";
-import { CategoriaModule } from "./categoria/categoria.module";
-import { Categoria } from "./categoria/entities/categoria.entity";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ExercicioModule } from './exercicio/exercicio.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { CategoriaModule } from './categoria/categoria.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
-    imports: [
-      TypeOrmModule.forRoot({
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'db_fitnessapp',
-        entities: [Exercicio, Categoria, Usuario],
-        synchronize: true,
-      }),
-      ExercicioModule,
-      CategoriaModule,
-      UsuarioModule,
-    ],
-    controllers: [],
-    providers: [],
-  })
-  export class AppModule {}
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
+    }),
+    ExercicioModule,
+    CategoriaModule,
+    UsuarioModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
